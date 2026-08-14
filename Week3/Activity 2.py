5a09d1d9d4f8129e6fa25cfe8c6b5e500acf602d
willtbing <115533299+willtbing@users.noreply.github.com>
2026-08-15 10:50:17 +1200

    Week 3 Activity 2

---
+++ Week3/Activity 2.py
@@ -0,0 +1,26 @@
+filename = "/Users/wupei/Documents/GitHub/YoobeeMSE800/Week3/junk.txt"
+#Open, read, and process the attached data file. 
+with open(filename, "r") as data:
+    lines = data.readlines()
+#Calculate and report the total number of lines in the file.
+print("The total number of lines in the file.")
+print(len(lines))
+#Add a new line at the end of the file containing exactly: `text file nanalyssis`
+text = ''.join(lines)
+print("------------------------------------")
+print("The original data:")
+print(text)
+text += "text file nanalyssis\n"
+with open(filename, "w") as data:
+    data.write(text)
+print("------------------------------------")
+print("The updated data after adding one more record:")
+print(text)
+#Convert all text in the `junk.txt` file to lowercase.
+text = text.lower()
+with open(filename, "w") as data:
+    data.write(text)
+print("------------------------------------")
+print("The updated data after converting to lowercase:")
+print(text)
+data.close()
